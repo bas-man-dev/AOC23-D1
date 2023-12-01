53ca17752ffba4037fe17d8f423763444278ae07
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2023-12-01 16:54:22 +0000

    venv

--- snowflake.py
+++ snowflake.py
@@ -1,6 +1,8 @@
 from collections import deque
 import re
 
+result = 0
+
 # Load file so we can work through all the data
 with open("data.txt") as my_file:
     for line in my_file:
@@ -14,9 +16,11 @@ with open("data.txt") as my_file:
         if len(numbers_string) < 1:
             pass 
         elif len(numbers_string) > 1:
-            print(f"{numbers_string[0]} + {numbers_string[-1]}")
+            result += (int(f"{numbers_string[0] + numbers_string[-1]}"))
         else:
-            print(f"{numbers_string[0]} + {numbers_string[0]}")
+            result += (int(f"{numbers_string[0] + numbers_string[0]}"))
+    
+        print(result)
             
 
 
